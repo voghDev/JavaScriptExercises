756826980d847a0ed71b57f23ea2878178ef99a5
Olmo Gallegos <vendofiatpunto@hotmail.com>
2023-02-09 22:33:54 +0100

    test: Add fix to failing test

--- src/__tests__/potter.test.js
+++ src/__tests__/potter.test.js
@@ -20,17 +20,55 @@ test('should apply simple discounts', () => {
   expect(calculatePrice([0, 1, 2, 3, 4])).toBe(oneBookPrice * 5 * 0.75);
 })
 
-export function calculatePrice(cart) {
-    if (cart.length == 1)
-        return oneBookPrice;
-    if (cart.length == 2)
-        return oneBookPrice * 2 * 0.95;
-    if (cart.length == 3)
-        return oneBookPrice * 3 * 0.9;
-    if (cart.length == 4)
-        return oneBookPrice * 4 * 0.8;
-    if (cart.length == 5)
-        return oneBookPrice * 5 * 0.75;
+test('should apply several discounts for various books', () => {
+  expect(calculatePrice([0, 0, 1])).toBe(oneBookPrice + (oneBookPrice * 2 * 0.95));
+  expect(calculatePrice([0, 0, 1, 1])).toBe(2 * (oneBookPrice * 2 * 0.95));
+  expect(calculatePrice([0, 0, 1, 2, 2, 3])).toBe((oneBookPrice * 4 * 0.8) + (oneBookPrice * 2 * 0.95));
+  expect(calculatePrice([0, 1, 1, 2, 3, 4])).toBe(oneBookPrice + (oneBookPrice * 5 * 0.75));
+});
 
-    return 0;
+export function calculatePrice(cart) {
+    const booksMap = countBooks(cart);
+    const result = calculatePriceBasedOnTheNumberOfBooks(booksMap);
+    return result;
+}
+
+function countBooks(cart) {
+    const booksMap = new Map();
+    cart.forEach(book => {
+        if (booksMap.has(book)) {
+            const value = booksMap.get(book);
+            booksMap.set(book, value + 1);
+        } else {
+            booksMap.set(book, 1);
+        }
+    });
+    return Array.of(...booksMap.values());
+}
+
+function calculatePriceBasedOnTheNumberOfBooks(books) {
+    const numberOfDifferentBooks = books.filter(x => x > 0).length;
+    if (numberOfDifferentBooks === 0) {
+        return 0.0;
+    } else if (numberOfDifferentBooks === 1) {
+        return oneBookPrice * books[0];
+    } else if (numberOfDifferentBooks === 2) {
+        return (oneBookPrice * 2 * 0.95 +
+            calculatePriceBasedOnTheNumberOfBooks(removeDiscountedBooks(books)));
+    } else if (numberOfDifferentBooks === 3) {
+        return (oneBookPrice * 3 * 0.9 +
+            calculatePriceBasedOnTheNumberOfBooks(removeDiscountedBooks(books)));
+    } else if (numberOfDifferentBooks === 4) {
+        return (oneBookPrice * 4 * 0.8 +
+            calculatePriceBasedOnTheNumberOfBooks(removeDiscountedBooks(books)));
+    } else if(numberOfDifferentBooks === 5) {
+        return (oneBookPrice * 5 * 0.75 +
+            calculatePriceBasedOnTheNumberOfBooks(removeDiscountedBooks(books)));
+    }
+
+    return 0.0;
+}
+
+function removeDiscountedBooks(books) {
+    return books.map(x => x - 1).filter(x => x > 0);
 }
